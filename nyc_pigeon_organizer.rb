105bdfbac560478e0994ec8c8698877a2e6b4790
github username <github email address>
2019-01-28 13:03:59 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  data.each do |key, value|
+    value.each do |type, pigeon|
+      pigeon.each do|name|
+        if hash.has_key?(name) == false
+          hash[name] = {
+            :color => [],
+            :gender => [],
+            :lives => []
+             }
+        end
+      end
+    end
+  end
+  data.each do |key, value|
+    value.each do |type, pigeon|
+      pigeon.each do |name|
+        hash[name][key].push(type.to_s)
+      end
+    end
+  end
+  hash
 end
