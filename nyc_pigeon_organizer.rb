a4931e7f71396cf80c4b6ccc27885dd8bb9ed2c4
Alonso Ortega <alonsojortega@gmail.com>
2019-02-07 21:25:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do |trait, options|
+    options.each do |option, pigeons|
+      pigeons.each do |pigeon|
+        new_hash[pigeon] ||= {}
+        new_hash[pigeon][trait] ||= []
+        new_hash[pigeon][trait].push(option.to_s)
+      end
+    end
+  end
+
+  new_hash
 end
