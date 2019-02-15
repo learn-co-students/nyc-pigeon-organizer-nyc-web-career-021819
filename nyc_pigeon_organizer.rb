f4ed978ef19a2d82037db55f18bcefa09e241e70
tchang <tlchang9@gmail.com>
2019-02-15 02:36:45 -0500

    Wrote #nyc_pigeon_organizer

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  solution = {}
+
+  data[:color].each do |color, names|
+    names.each do |name|
+      if solution[name] == nil
+        solution[name] = {:color => [color.to_s]}
+      else solution[name].has_key?(:color)
+        solution[name][:color].push(color.to_s)
+      end
+    end
+  end
+
+  data[:gender].each do |gender, names|
+    names.each do |name|
+      solution[name][:gender] = [gender.to_s]
+    end
+  end
+
+  data[:lives].each do |location, names|
+    names.each do |name|
+      solution[name][:lives] = [location]
+    end
+  end
+  return solution
 end
